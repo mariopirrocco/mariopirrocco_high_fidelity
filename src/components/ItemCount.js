35bed72b2f80ac8612763e6337e9334e3b1ab669
mariopirrocco <91997819+mariopirrocco@users.noreply.github.com>
2022-03-07 18:34:30 -0300

    Desafío contador con botón

---
+++ src/components/ItemCount.js
@@ -0,0 +1,32 @@
+import './itemCount.css'
+import { useState } from 'react'
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+
+	const [ counter, setCounter ] = useState(initial)
+
+	const addItem = () => {
+		if(counter < stock) {
+			setCounter(counter + 1)
+		}
+	}
+
+	const removeItem = () => {
+		if(counter > initial) {
+			setCounter(counter - 1)
+		}
+	}
+	return (
+		<>
+			<div className="counter">{ counter }</div>
+			<div className="control">
+				<button className="add-item" onClick = {addItem}> + </button>
+				<button className="remove-item" onClick = {removeItem}> - </button>	
+			</div>
+			
+			<button className="add-to-cart" onClick={()=> onAdd(counter)}>Agregar al carrito</button>
+		</>
+	)
+}
+
+export default ItemCount
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,20 @@
+import ItemCount from './ItemCount'
+
 const ItemListContainer = (props) => {
+
+	let stock = 8
+	const initial = 1
+	const onAdd = (value) => {
+		if(value === stock) {
+			console.log(`Has cargado ${value} discos en tu carrito, es todo el stock que tenemos en este momento`)
+		} else {
+			console.log(`Tienes ${value} disco(s) en tu carrito`)
+		}		
+	}
+
 	return(
 		<div className="item-list-container">
-			<h3>Bienvenido <strong>{props.greeting} a HIGH FIDELITY</strong> <br />la tienda de discos usados más grande de Sudamérica</h3>
+			<ItemCount stock={stock} initial={initial} onAdd={onAdd} />
 		</div>
 	)
 }
